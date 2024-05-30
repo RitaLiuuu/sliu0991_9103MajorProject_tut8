8b0ec8a70e66c3bc0ffb63b81eb7cc2c3980495d
RitaLiuuu <168101469+RitaLiuuu@users.noreply.github.com>
2024-05-30 22:10:39 +1000

    Add new animation and update readmefile
    -Adding random colour
    -Animating the size of the biggest circles
    -Animating the size of the wavy lines

--- README.md
+++ README.md
@@ -2,23 +2,29 @@
 
 # Part 1: Instruction
 
-1. Click the button on the bottom of the page and wait the music to start. 
+1. Refresh the page to get random colour big circles.
+2. Click the button on the bottom of the page and wait the music to start. 
 
 # Part 2: Details of my individual approach to animating the group code.
 
 1. I chose audio to use the level or frequency content of an audio track to animate my group work.
-2. The wavyline on the background will be animated to move with the music. The rhythm will create unique effect as the stroke of the lines is continuously changing and the scale is changing which are different to my group members. 
+2. -The scale of the concentric circles will be changed dynamically with the music. 
+-The colour of the biggest circle will be random through refreshing the page
+-And the wavyline on the background will also be animated to move with the music. 
+-The rhythm will create unique effect as the stroke of the lines is continuously changing and the scale is changing which are different to my group members. 
 
 # Part 3: Inspiration and References
 ![Imaging Inspiration1](assets/wave1.jpg)
 (Holly Sanders art. (n.d.). Holly Sanders art [Illustration]. Pinterest. https://www.pinterest.com.au/pin/70437487889304/)
 ![Imaging Inspiration2](assets/inspiration1.jpg)
 (Kelman, A. (2017, July 12). 40 Complex Yet Beautiful Aboriginal Art Examples [Artwork]. Bored Art. https://www.boredart.com/2017/07/complex-yet-beautiful-aboriginal-art-examples.html)
+![Imaging Inspiration3](assets/inspiration2.jpg)
+(Interior design(n.d.). Korean Crisis Finds Expression in John Marx Drawing - Interior Design. Pinterest. https://www.pinterest.com.au/pin/405957353896499040/)
 
-The inspiration came from the Aboriginal art, as I want to present the moves of the wave. The scaling of the wave should be changed through the background music.
+The inspiration came from the Aboriginal art, as I want to present the moves of the wave. The scaling of the wave should be changed through the background music. And I got some inspiration from the circle art. By making the circle size changed with the music, I can create the effect of circles overlays with each other.
 
 I also got inspiration from week 11 tutorial on Canvas, as it taught me how to load a sound file and play back.
 
 # Part 4: Technical explanation
-I preloaded a sound file as the background music, and then added a button to play or pause. Then I used amplitude analyzer to measure the volume of the music, to animate the size and the stroke of the wavy lines in the background.
+I preloaded a sound file as the background music, and then added a button to play or pause. I set the colour of the biggest circle to random using rgb colour from 0-255. Then I used amplitude analyzer to measure the volume of the music, to animate the size and the stroke of the wavy lines in the background. After that, I use 'rms' value to adjust 'circlediameter' and pass the changing to the biggest circle class to make it move with the sound.  
 
--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 let circleDiameter;
 let circles = []; // Array for big circles
-let bgCircles = []; // Array for all the small background circles
-let bgCircleAmount = 400;
-
-//audio
+let redCircles = []; // Array for small red circles
+let redCircleAmount = 400;
 let song, analyzer;
+let fft;
+let numBins = 128;
+let smoothing = 0.8;
+let button;
 
 function preload() {
-  //audio file from freesound https://freesound.org/people/multitonbits/sounds/383935/?
-  //licensed under the Creative Commons 0 License
-  //let's load the sound file in preload
+  //audio file from freesound https://uppbeat.io/browse/music/hip-hop-beats
   song = loadSound('assets/backgroundmusic.mp3');
 }
 
@@ -17,49 +17,35 @@ function preload() {
 let wavylineX = [2.8, 8.9, 14.9, 0.7, 6.8, 12.7, 19.2, -0.3, 5.8, 11.5, 17.4, 4.3, 10, 16];
 let wavylineY = [2.7, 1, 0, 8.9, 7.7, 6.8, 4.2, 15.2, 13.5, 12.8, 10.5, 19.5, 18.5, 17];
 
-// Predefined colors for the big circles
-let circleColors = [
-  [210, 266, 248],
-  [250, 181, 37],
-  [255, 231, 233],
-  [237, 213, 148],
-  [220, 255, 237],
-  [252, 179, 138],
-  [239, 174, 49],
-  [197, 254, 254],
-  [251, 222, 131],
-  [255, 235, 245],
-  [255, 249, 239],
-  [255, 247, 248],
-  [255, 208, 139],
-  [253, 220, 62]
-];
-
 // function to create the lines in the background, adapted from https://editor.p5js.org/zygugi/sketches/BJHK3O_yM
-function wavyLines(linesX, linesY, lineWeight, lineR, lineG, lineB) {
+function wavyLines(linesX, linesY, lineWeight, lineR, lineG, lineB, rms) {
+  // styling for the lines
   noFill();
   stroke(lineR, lineG, lineB);
   strokeWeight(lineWeight);
 
   beginShape();
+
+  // variables that change the noise level for the lines
   var xoff = 0;
   var yoff = 0.0;
   let noiseY = 0.05;
-  let radius = (windowHeight / 20) * 4.2;
+  let radius = (windowHeight / 20) * 4.2 * (1 + rms); // Dynamic radius based on rms
 
+  // loop that creates the lines
   for (var a = 0; a < TWO_PI; a += 0.1) {
-    var offset = map(noise(xoff, noiseY), 0, 1, -15, 5);
+    var offset = map(noise(xoff, noiseY), 0, 1, -15, 5) * (1 + rms);
     var r = radius + offset;
-    var x = (windowHeight/20)*linesX + 0.8*r * cos(a);
-    var y = (windowHeight/20)*linesY + 0.8*r * sin(a);
+    var x = (windowHeight / 20) * linesX + 0.8 * r * cos(a);
+    var y = (windowHeight / 20) * linesY + 0.8 * r * sin(a);
     vertex(x, y);
     xoff += 0.5;
   }
   endShape();
 }
 
-// Class for small random background circles
-class BgCirclePattern {
+// Class for small random red circles
+class RedCirclePattern {
   constructor(xPos, yPos, radius) {
     this.xPos = xPos;
     this.yPos = yPos;
@@ -75,11 +61,11 @@ class BgCirclePattern {
 
 // Class for the biggest Circle Pattern
 class CirclePattern {
-  constructor(xFactor, yFactor, color) {
+  constructor(xFactor, yFactor) {
     this.xFactor = xFactor;
     this.yFactor = yFactor;
     this.smallCircles = this.generateRandomSmallCircles();
-    this.colour = color;
+    this.colour = [random(0, 255), random(0, 255), random(0, 255)];
     // Generate random colors for nested circles
     this.r2Color = [random(0, 255), random(0, 255), random(0, 255)];
     this.r3Color = [random(0, 255), random(0, 255), random(0, 255)];
@@ -92,40 +78,42 @@ class CirclePattern {
     }
   }
 
-  display() {
+  display(diameter) {
     fill(this.colour);
     let x = this.xFactor * windowHeight / 20;
     let y = this.yFactor * windowHeight / 20;
-    circle(x, y, circleDiameter);
+    circle(x, y, diameter);
 
     // draw random small circles
     this.drawRandomSmallCircles();
 
     // draw nested circles
-    this.drawNestedCircles(x, y);
+    this.drawNestedCircles(x, y, diameter);
   }
 
-  drawNestedCircles(x, y) {
-    // second circle
-    let r2 = windowHeight / 20 * 1.5;
+  drawNestedCircles(x, y, diameter) {
+    // Calculate dynamic radii based on the provided diameter
+    let dynamicScale = diameter / circleDiameter;
+    let r2 = windowHeight / 20 * 1.5 * dynamicScale;
+    let r3 = windowHeight / 20 * 1.35 * dynamicScale;
+    let r4 = windowHeight / 20 * 0.5 * dynamicScale;
+
     fill(this.r2Color);
     circle(x, y, r2 * 2);
 
-    let r3 = windowHeight / 20 * 1.35;
     fill(this.r3Color);
     circle(x, y, r3 * 2);
 
     // Additional rings between r3 and r4
-    let r4 = windowHeight / 20 * 0.5;
-    let ringRadii = [ // Radius of each circle between first circle 3 and circle 4
-      windowHeight / 20 * 1.2,
-      windowHeight / 20 * 1.0,
-      windowHeight / 20 * 0.8,
-      windowHeight / 20 * 0.6,
-      windowHeight / 20 * 0.4
+    let ringRadii = [
+      windowHeight / 20 * 1.2 * dynamicScale,
+      windowHeight / 20 * 1.0 * dynamicScale,
+      windowHeight / 20 * 0.8 * dynamicScale,
+      windowHeight / 20 * 0.6 * dynamicScale,
+      windowHeight / 20 * 0.4 * dynamicScale
     ];
     for (let i = 0; i < ringRadii.length; i++) {
-      fill(this.additionalRingColors[i]); // Achieve different color fills by calling the data in the array
+      fill(this.additionalRingColors[i]);
       circle(x, y, ringRadii[i] * 2);
     }
   }
@@ -188,39 +176,49 @@ class CirclePattern {
 function setup() {
   createCanvas(windowHeight, windowHeight);
   background(5, 89, 127);
-  circleDiameter = (windowHeight / 20) * 5.5;
+
+  fft = new p5.FFT(smoothing, numBins);
+  song.connect(fft);
 
   // create a new Amplitude analyzer, this will analyze the volume of the song
   analyzer = new p5.Amplitude();
-
-  // Connect the input of the analyzer to the song
   analyzer.setInput(song);
 
-  //Add a button for play/pause
-  let button = createButton('Play/Pause');
-
-  //set the position of the button to the bottom center
+  // Add a button for play/pause
+  button = createButton("Play/Pause");
   button.position((width - button.width) / 2, height - button.height - 2);
-
   button.mousePressed(play_pause);
 
-  // Initialize circles with their respective positions and colors
-  for (let i = 0; i < wavylineX.length; i++) {
-    circles.push(new CirclePattern(wavylineX[i], wavylineY[i], circleColors[i]));
-  }
+  circleDiameter = (windowHeight / 20) * 5.5;
 
-  // Create small background circles
-  for (let i = 0; i < bgCircleAmount; i++) {
+  // Initialize circles with their respective positions
+  circles.push(new CirclePattern(2.8, 2.7));
+  circles.push(new CirclePattern(8.9, 1));
+  circles.push(new CirclePattern(14.9, 0));
+  circles.push(new CirclePattern(0.7, 8.9));
+  circles.push(new CirclePattern(6.8, 7.7));
+  circles.push(new CirclePattern(12.7, 6.8));
+  circles.push(new CirclePattern(19.2, 4.2));
+  circles.push(new CirclePattern(-0.3, 15.2));
+  circles.push(new CirclePattern(5.8, 13.5));
+  circles.push(new CirclePattern(11.5, 12.8));
+  circles.push(new CirclePattern(17.4, 10.5));
+  circles.push(new CirclePattern(4.3, 19.5));
+  circles.push(new CirclePattern(10, 18.5));
+  circles.push(new CirclePattern(16, 17));
+
+  // Create small red circles
+  for (let i = 0; i < redCircleAmount; i++) {
     let overlapping = true;
-    let bgCircle;
+    let redCircle;
     while (overlapping) {
       overlapping = false;
-      bgCircle = new BgCirclePattern(random(width), random(height), random(0, 10));
+      redCircle = new RedCirclePattern(random(width), random(height), random(0, 10));
 
-      // Check for overlap with other small background circles
-      for (let other of bgCircles) {
-        let d = dist(bgCircle.xPos, bgCircle.yPos, other.xPos, other.yPos);
-        if (d < bgCircle.radius + other.radius) {
+      // Check for overlap with other small red circles
+      for (let other of redCircles) {
+        let d = dist(redCircle.xPos, redCircle.yPos, other.xPos, other.yPos);
+        if (d < redCircle.radius + other.radius) {
           overlapping = true;
           break;
         }
@@ -228,14 +226,14 @@ function setup() {
 
       // Check for overlap with big circles
       for (let bigCircle of circles) {
-        let d = dist(bgCircle.xPos, bgCircle.yPos, bigCircle.getX(), bigCircle.getY());
-        if (d < bgCircle.radius + circleDiameter / 2 + 15) {
+        let d = dist(redCircle.xPos, redCircle.yPos, bigCircle.getX(), bigCircle.getY());
+        if (d < redCircle.radius + circleDiameter / 2 + 15) {
           overlapping = true;
           break;
         }
       }
     }
-    bgCircles.push(bgCircle);
+    redCircles.push(redCircle);
   }
 }
 
@@ -244,24 +242,27 @@ function draw() {
 
   let rms = analyzer.getLevel();
 
+  // Adjust circleDiameter dynamically based on rms
+  let dynamicCircleDiameter = (windowHeight / 20) * 5.5 * (1 + rms);
+
   // draw lines
   for (let t = 0; t < wavylineX.length; t++) {
-    wavyLines(wavylineX[t], wavylineY[t], 10 + rms * 5, 244, 198, 226);
+    wavyLines(wavylineX[t], wavylineY[t], 5 + rms * 5, 244, 198, 226, rms);
   }
 
   // draw inner lines
   for (let t = 0; t < wavylineX.length; t++) {
-    wavyLines(wavylineX[t], wavylineY[t], 5 + rms * 2, 134, 198, 226);
+    wavyLines(wavylineX[t], wavylineY[t], 2 + rms * 2, 134, 198, 226, rms);
   }
 
-  // Draw all small background circles
-  for (let bgCircle of bgCircles) {
-    bgCircle.display();
+  // Draw all small red circles
+  for (let redCircle of redCircles) {
+    redCircle.display();
   }
 
-  // Draw all big circles
+  // Draw all big circles with dynamic circleDiameter
   for (let circle of circles) {
-    circle.display();
+    circle.display(dynamicCircleDiameter);
   }
 }
 
@@ -269,8 +270,6 @@ function play_pause() {
   if (song.isPlaying()) {
     song.stop();
   } else {
-    //we can use song.play() here if we want the song to play once
-    //In this case, we want the song to loop, so we call song.loop()
     song.loop();
   }
 }
