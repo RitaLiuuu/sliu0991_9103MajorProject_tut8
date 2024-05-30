697e4633edd9421494769e0d8156ca0891051074
RitaLiuuu <168101469+RitaLiuuu@users.noreply.github.com>
2024-05-30 23:13:21 +1000

    Final code
    Adding the circular display of frequency band

--- README.md
+++ README.md
@@ -8,23 +8,34 @@
 # Part 2: Details of my individual approach to animating the group code.
 
 1. I chose audio to use the level or frequency content of an audio track to animate my group work.
-2. -The scale of the concentric circles will be changed dynamically with the music. 
--The colour of the biggest circle will be random through refreshing the page
--And the wavyline on the background will also be animated to move with the music. 
--The rhythm will create unique effect as the stroke of the lines is continuously changing and the scale is changing which are different to my group members. 
+2. - The scale of the concentric circles will be changed dynamically with the music. 
+- The colour of the biggest circle will be random through refreshing the page
+- And the wavyline on the background will also be animated to move with the music. 
+- The rhythm will create unique effect as the stroke of the lines is continuously changing and the scale is changing which are different to my group members. 
+- Creating circular frequency bands that has the gradient colour from white to orange around r2 circle.
 
 # Part 3: Inspiration and References
 ![Imaging Inspiration1](assets/wave1.jpg)
-(Holly Sanders art. (n.d.). Holly Sanders art [Illustration]. Pinterest. https://www.pinterest.com.au/pin/70437487889304/)
+
 ![Imaging Inspiration2](assets/inspiration1.jpg)
-(Kelman, A. (2017, July 12). 40 Complex Yet Beautiful Aboriginal Art Examples [Artwork]. Bored Art. https://www.boredart.com/2017/07/complex-yet-beautiful-aboriginal-art-examples.html)
+
 ![Imaging Inspiration3](assets/inspiration2.jpg)
-(Interior design(n.d.). Korean Crisis Finds Expression in John Marx Drawing - Interior Design. Pinterest. https://www.pinterest.com.au/pin/405957353896499040/)
 
-The inspiration came from the Aboriginal art, as I want to present the moves of the wave. The scaling of the wave should be changed through the background music. And I got some inspiration from the circle art. By making the circle size changed with the music, I can create the effect of circles overlays with each other.
+![Imaging Inspiration4](assets/inspiration3.jpg)
 
-I also got inspiration from week 11 tutorial on Canvas, as it taught me how to load a sound file and play back.
+The inspiration came from the Aboriginal art, as I want to present the moves of the wave. The scaling of the wave should be changed through the background music. And I got some inspiration from the circle art. By making the circle size changed with the music, I can create the effect of circles overlays with each other. As I am animating with sound, I would to visualise the frequency band as a circular shape around the inner circle.
+
+I also got inspiration from week 11 tutorial on Canvas, as it taught me how to use sound file controlling shape class and add circular frequency bands using FFT. 
 
 # Part 4: Technical explanation
-I preloaded a sound file as the background music, and then added a button to play or pause. I set the colour of the biggest circle to random using rgb colour from 0-255. Then I used amplitude analyzer to measure the volume of the music, to animate the size and the stroke of the wavy lines in the background. After that, I use 'rms' value to adjust 'circlediameter' and pass the changing to the biggest circle class to make it move with the sound.  
+I preloaded a sound file as the background music, and then added a button to play or pause. I set the colour of the biggest circle to random using rgb colour from 0-255. Then I used amplitude analyzer to measure the volume of the music, to animate the size and the stroke of the wavy lines in the background. After that, I use 'rms' value to adjust 'circlediameter' and pass the changing to the biggest circle class to make it move with the sound. At the end, I added a circular display base on the frequency band using FFT with smooth transitions. Using the base radius for the inner circle to identify the starting place of the frequency bands.And then I created gradient colour from white to orange using 'map' function. 
 
+# Part 5: References
+Holly Sanders art. (n.d.). Holly Sanders art [Illustration]. Pinterest. https://www.pinterest.com.au/pin/70437487889304/
+
+Kelman, A. (2017, July 12). 40 Complex Yet Beautiful Aboriginal Art Examples [Artwork]. Bored Art. 
+https://www.boredart.com/2017/07/complex-yet-beautiful-aboriginal-art-examples.html
+
+Interior design. (n.d.). Korean Crisis Finds Expression in John Marx Drawing - Interior Design [Illustration]. Pinterest. https://www.pinterest.com.au/pin/405957353896499040/
+
+Vecteezy. (n.d.). Circle audio wave. Circular music sound equalizer. Abstract radial radio and voice volume symbol [Illustration]. Pinterest. https://www.pinterest.com.au/pin/328410997843467909/
--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 let circleDiameter;
 let circles = []; // Array for big circles
-let redCircles = []; // Array for small red circles
-let redCircleAmount = 400;
+let bgCircles = []; // Array for small red circles
+let bgCircleAmount = 400;
+//for audio
 let song, analyzer;
 let fft;
 let numBins = 128;
@@ -28,7 +29,7 @@ function wavyLines(linesX, linesY, lineWeight, lineR, lineG, lineB, rms) {
 
   // variables that change the noise level for the lines
   var xoff = 0;
-  var yoff = 0.0;
+  var yoff = 0;
   let noiseY = 0.05;
   let radius = (windowHeight / 20) * 4.2 * (1 + rms); // Dynamic radius based on rms
 
@@ -45,7 +46,7 @@ function wavyLines(linesX, linesY, lineWeight, lineR, lineG, lineB, rms) {
 }
 
 // Class for small random red circles
-class RedCirclePattern {
+class bgCirclePattern {
   constructor(xPos, yPos, radius) {
     this.xPos = xPos;
     this.yPos = yPos;
@@ -79,6 +80,7 @@ class CirclePattern {
   }
 
   display(diameter) {
+    noStroke();
     fill(this.colour);
     let x = this.xFactor * windowHeight / 20;
     let y = this.yFactor * windowHeight / 20;
@@ -101,6 +103,9 @@ class CirclePattern {
     fill(this.r2Color);
     circle(x, y, r2 * 2);
 
+    // Draw frequency bands around r2
+    this.drawFrequencyBands(x, y, r2);
+
     fill(this.r3Color);
     circle(x, y, r3 * 2);
 
@@ -118,6 +123,32 @@ class CirclePattern {
     }
   }
 
+  // Draw circular frequency bands 
+  drawFrequencyBands(x, y, radius) {
+    let spectrum = fft.analyze();
+    //the distance between each frequency band segment in the circle
+    let angleStep = TWO_PI / numBins;
+    for (let i = 0; i < numBins; i++) {
+      let angle = i * angleStep;
+      let amp = spectrum[i];
+      let bandRadius = map(amp, 0, 255, radius, radius * 1.5);
+      let x1 = x + radius * cos(angle);
+      let y1 = y + radius * sin(angle);
+      let x2 = x + bandRadius * cos(angle);
+      let y2 = y + bandRadius * sin(angle);
+
+      // Create a gradient color from white to orange
+      let r = map(i, 0, numBins, 255, 255);
+      let g = map(i, 0, numBins, 255, 165);
+      let b = map(i, 0, numBins, 255, 0);
+      stroke(r, g, b);
+
+      line(x1, y1, x2, y2);
+    }
+  }
+
+  //creates an array of small circles that are placed randomly within a larger circle
+  //This code was adapted from https://editor.p5js.org/slow_izzm/sketches/HyqLs-7AX */
   generateRandomSmallCircles() {
     let smallCircles = [];
     let x = this.xFactor * windowHeight / 20;
@@ -145,6 +176,7 @@ class CirclePattern {
     return smallCircles;
   }
 
+  //check the samll circles are not overlaping with each other
   isValidPosition(newCircle, smallCircles, diameter) {
     for (let circle of smallCircles) {
       let distance = dist(newCircle.x, newCircle.y, circle.x, circle.y);
@@ -155,9 +187,10 @@ class CirclePattern {
     return true;
   }
 
+  //draw random small circles inside biggest circle
   drawRandomSmallCircles() {
-    let smallCircleDiameter = 10; // Smaller size of small circles
-    noStroke(); // Remove stroke
+    let smallCircleDiameter = 10; 
+    noStroke(); 
     for (let smallCircle of this.smallCircles) {
       fill(smallCircle.color);
       circle(smallCircle.x, smallCircle.y, smallCircleDiameter);
@@ -177,6 +210,7 @@ function setup() {
   createCanvas(windowHeight, windowHeight);
   background(5, 89, 127);
 
+  //Create a new instance of p5.FFT() object
   fft = new p5.FFT(smoothing, numBins);
   song.connect(fft);
 
@@ -191,7 +225,7 @@ function setup() {
 
   circleDiameter = (windowHeight / 20) * 5.5;
 
-  // Initialize circles with their respective positions
+  // Initialise circles with their respective positions
   circles.push(new CirclePattern(2.8, 2.7));
   circles.push(new CirclePattern(8.9, 1));
   circles.push(new CirclePattern(14.9, 0));
@@ -207,33 +241,33 @@ function setup() {
   circles.push(new CirclePattern(10, 18.5));
   circles.push(new CirclePattern(16, 17));
 
-  // Create small red circles
-  for (let i = 0; i < redCircleAmount; i++) {
+  // Create small background circles
+  for (let i = 0; i < bgCircleAmount; i++) {
     let overlapping = true;
-    let redCircle;
+    let bgCircle;
     while (overlapping) {
       overlapping = false;
-      redCircle = new RedCirclePattern(random(width), random(height), random(0, 10));
+      bgCircle = new bgCirclePattern(random(width), random(height), random(0, 10));
 
-      // Check for overlap with other small red circles
-      for (let other of redCircles) {
-        let d = dist(redCircle.xPos, redCircle.yPos, other.xPos, other.yPos);
-        if (d < redCircle.radius + other.radius) {
-          overlapping = true;
-          break;
-        }
-      }
-
-      // Check for overlap with big circles
-      for (let bigCircle of circles) {
-        let d = dist(redCircle.xPos, redCircle.yPos, bigCircle.getX(), bigCircle.getY());
-        if (d < redCircle.radius + circleDiameter / 2 + 15) {
-          overlapping = true;
-          break;
-        }
+  // Check for overlap with other background circles
+  for (let other of bgCircles) {
+    let d = dist(bgCircle.xPos, bgCircle.yPos, other.xPos, other.yPos);
+      if (d < bgCircle.radius + other.radius) {
+        overlapping = true;
+        break;
       }
     }
-    redCircles.push(redCircle);
+
+      // Check for overlap with big circles
+  for (let bigCircle of circles) {
+    let d = dist(bgCircle.xPos, bgCircle.yPos, bigCircle.getX(), bigCircle.getY());
+      if (d < bgCircle.radius + circleDiameter / 2 + 15) {
+        overlapping = true;
+        break;
+      }
+    }
+  }
+  bgCircles.push(bgCircle);
   }
 }
 
@@ -245,19 +279,19 @@ function draw() {
   // Adjust circleDiameter dynamically based on rms
   let dynamicCircleDiameter = (windowHeight / 20) * 5.5 * (1 + rms);
 
-  // draw lines
+  // draw wavy lines
   for (let t = 0; t < wavylineX.length; t++) {
     wavyLines(wavylineX[t], wavylineY[t], 5 + rms * 5, 244, 198, 226, rms);
   }
 
-  // draw inner lines
+  // draw inner wavy lines
   for (let t = 0; t < wavylineX.length; t++) {
     wavyLines(wavylineX[t], wavylineY[t], 2 + rms * 2, 134, 198, 226, rms);
   }
 
-  // Draw all small red circles
-  for (let redCircle of redCircles) {
-    redCircle.display();
+  // Draw all small background circles
+  for (let bgCircle of bgCircles) {
+    bgCircle.display();
   }
 
   // Draw all big circles with dynamic circleDiameter
@@ -266,6 +300,7 @@ function draw() {
   }
 }
 
+//play or pause the music
 function play_pause() {
   if (song.isPlaying()) {
     song.stop();
@@ -276,6 +311,8 @@ function play_pause() {
 
 function windowResized() {
   resizeCanvas(windowHeight, windowHeight);
-  circleDiameter = (windowHeight / 20) * 5.5; // Recalculate circleDiameter after resize
-  draw(); // Redraw circles to reflect the new dimensions
+  circleDiameter = (windowHeight / 20) * 5.5; 
+  draw(); 
+  //Reset the position of the button
+  button.position((width - button.width) / 2, height - button.height - 2);
 }
